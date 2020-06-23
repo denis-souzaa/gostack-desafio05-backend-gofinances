d7a2373e604ec23dc638b5366c79a4238125e37e
Denis Souza <denyssouza1@hotmail.com>
2020-06-22 21:16:27 -0300

    finish challenge

---
+++ readme.md
@@ -0,0 +1,3 @@
+## Backend GoFinances
+
+Desafio 05 da GoStack consiste em criar o backend da aplicação GoFinances, utilizando os conceitos de NodeJS com Typescript.
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,18 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Transaction type is not valid');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw Error('The Saldo not is sufficient to this transaction');
+    }
+
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
